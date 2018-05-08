cc84836389b9e67a05ea7ad2988665b9326cb721
PriyamAgrawal <priyamagrawal2208@gmail.com>
2018-05-08 12:13:55 +0530

    removedd sample

--- views/js/voting.js
+++
@@ -1,6 +0,0 @@
-function addRest(){
-	var database = firebase.database();
-	var restaurantRef = database.ref('/Restaurants');
-	var restaurant = document.getElementById('addrest').value;
-	restaurantRef.push(restaurant);
-}
